1a2b60c7ea23abdd7108e092880af278a2fc6bd0
Meng <edmonluan@gmail.com>
2016-03-02 01:06:42 -0400

    Format code.

--- src/GreedyMCPlayer.java
+++ src/GreedyMCPlayer.java
@@ -1,24 +1,29 @@
+
 import java.util.ArrayList;
 import java.util.Random;
 
 /**
- * GreedyMCPlayer - a simple, greedy Monte Carlo implementation of the player interface for PokerSquares.
- * For each possible play, continues greedy play with random possible card draws to a given depth limit 
- * (or game end).  Having sampled trajectories for all possible plays, the GreedyMCPlayer then selects the
- * play yielding the best average scoring potential in such Monte Carlo simulation.
- * 
- * Disclaimer: This example code is not intended as a model of efficiency. (E.g., patterns from Knuth's Dancing Links
- * algorithm (DLX) can provide faster legal move list iteration/deletion/restoration.)  Rather, this example
- * code illustrates how a player could be constructed.  Note how time is simply managed so as to not run out the play clock.
- * 
+ * GreedyMCPlayer - a simple, greedy Monte Carlo implementation of the player
+ * interface for PokerSquares. For each possible play, continues greedy play
+ * with random possible card draws to a given depth limit (or game end). Having
+ * sampled trajectories for all possible plays, the GreedyMCPlayer then selects
+ * the play yielding the best average scoring potential in such Monte Carlo
+ * simulation.
+ *
+ * Disclaimer: This example code is not intended as a model of efficiency.
+ * (E.g., patterns from Knuth's Dancing Links algorithm (DLX) can provide faster
+ * legal move list iteration/deletion/restoration.) Rather, this example code
+ * illustrates how a player could be constructed. Note how time is simply
+ * managed so as to not run out the play clock.
+ *
  * Author: Todd W. Neller
  */
 public class GreedyMCPlayer implements PokerSquaresPlayer {
-    
+
     private final int SIZE = 5; // number of rows/columns in square grid
     private final int NUM_POS = SIZE * SIZE; // number of positions in square grid
     private final int NUM_CARDS = Card.NUM_CARDS; // number of cards in deck
-    private Random random = new Random(); // pseudorandom number generator for Monte Carlo simulation 
+    private Random random = new Random(); // pseudorandom number generator for Monte Carlo simulation
     private int[] plays = new int[NUM_POS]; // positions of plays so far (index 0 through numPlays - 1) recorded as integers using row-major indices.
     // row-major indices: play (r, c) is recorded as a single integer r * SIZE + c (See http://en.wikipedia.org/wiki/Row-major_order)
     // From plays index [numPlays] onward, we maintain a list of yet unplayed positions.
@@ -27,8 +32,8 @@ public class GreedyMCPlayer implements PokerSquaresPlayer {
     private int depthLimit = 2; // default depth limit for Greedy Monte Carlo (MC) play
     private Card[][] grid = new Card[SIZE][SIZE]; // grid with Card objects or null (for empty positions)
     private Card[] simDeck = Card.getAllCards(); // a list of all Cards. As we learn the index of cards in the play deck,
-                                                 // we swap each dealt card to its correct index.  Thus, from index numPlays 
-                                                 // onward, we maintain a list of undealt cards for MC simulation.
+    // we swap each dealt card to its correct index.  Thus, from index numPlays
+    // onward, we maintain a list of undealt cards for MC simulation.
     private int[][] legalPlayLists = new int[NUM_POS][NUM_POS]; // stores legal play lists indexed by numPlays (depth)
     // (This avoids constant allocation/deallocation of such lists during the greedy selections of MC simulations.)
 
@@ -37,29 +42,34 @@ public class GreedyMCPlayer implements PokerSquaresPlayer {
      */
     public GreedyMCPlayer() {
     }
-    
+
     /**
-     * Create a Greedy Monte Carlo player that simulates greedy play to a given depth limit.
+     * Create a Greedy Monte Carlo player that simulates greedy play to a given
+     * depth limit.
+     *
      * @param depthLimit depth limit for random greedy simulated play
      */
     public GreedyMCPlayer(int depthLimit) {
         this.depthLimit = depthLimit;
     }
-    
+
     /* (non-Javadoc)
      * @see PokerSquaresPlayer#init()
      */
     @Override
-    public void init() { 
+    public void init() {
         // clear grid
-        for (int row = 0; row < SIZE; row++)
-            for (int col = 0; col < SIZE; col++)
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
                 grid[row][col] = null;
+            }
+        }
         // reset numPlays
         numPlays = 0;
         // (re)initialize list of play positions (row-major ordering)
-        for (int i = 0; i < NUM_POS; i++)
+        for (int i = 0; i < NUM_POS; i++) {
             plays[i] = i;
+        }
     }
 
     /* (non-Javadoc)
@@ -74,18 +84,19 @@ public class GreedyMCPlayer implements PokerSquaresPlayer {
          *     partially-filled) grid is scored.
          *   For each greedy play simulation, random undrawn cards are drawn in simulation and the greedy player
          *     picks a play position that maximizes the score (breaking ties randomly).
-         *   After many such plays, the average score per simulated play is computed.  The play with the highest 
-         *     average score is chosen (breaking ties randomly).   
+         *   After many such plays, the average score per simulated play is computed.  The play with the highest
+         *     average score is chosen (breaking ties randomly).
          */
-        
-        // match simDeck to actual play event; in this way, all indices forward from the card contain a list of 
+
+        // match simDeck to actual play event; in this way, all indices forward from the card contain a list of
         //   undealt Cards in some permutation.
         int cardIndex = numPlays;
-        while (!card.equals(simDeck[cardIndex]))
+        while (!card.equals(simDeck[cardIndex])) {
             cardIndex++;
+        }
         simDeck[cardIndex] = simDeck[numPlays];
         simDeck[numPlays] = card;
-        
+
         if (numPlays < 24) { // not the forced last play
             // compute average time per move evaluation
             int remainingPlays = NUM_POS - numPlays; // ignores triviality of last play to keep a conservative margin for game completion
@@ -94,7 +105,7 @@ public class GreedyMCPlayer implements PokerSquaresPlayer {
             // copy the play positions (row-major indices) that are empty
             System.arraycopy(plays, numPlays, legalPlayLists[numPlays], 0, remainingPlays);
             double maxAverageScore = Double.NEGATIVE_INFINITY; // maximum average score found for moves so far
-            ArrayList<Integer> bestPlays = new ArrayList<Integer>(); // all plays yielding the maximum average score 
+            ArrayList<Integer> bestPlays = new ArrayList<Integer>(); // all plays yielding the maximum average score
             for (int i = 0; i < remainingPlays; i++) { // for each legal play position
                 int play = legalPlayLists[numPlays][i];
                 long startTime = System.currentTimeMillis();
@@ -111,8 +122,9 @@ public class GreedyMCPlayer implements PokerSquaresPlayer {
                 // update (if necessary) the maximum average score and the list of best plays
                 double averageScore = (double) scoreTotal / simCount;
                 if (averageScore >= maxAverageScore) {
-                    if (averageScore > maxAverageScore)
+                    if (averageScore > maxAverageScore) {
                         bestPlays.clear();
+                    }
                     bestPlays.add(play);
                     maxAverageScore = averageScore;
                 }
@@ -120,8 +132,9 @@ public class GreedyMCPlayer implements PokerSquaresPlayer {
             int bestPlay = bestPlays.get(random.nextInt(bestPlays.size())); // choose a best play (breaking ties randomly)
             // update our list of plays, recording the chosen play in its sequential position; all onward from numPlays are empty positions
             int bestPlayIndex = numPlays;
-            while (plays[bestPlayIndex] != bestPlay)
+            while (plays[bestPlayIndex] != bestPlay) {
                 bestPlayIndex++;
+            }
             plays[bestPlayIndex] = plays[numPlays];
             plays[numPlays] = bestPlay;
         }
@@ -131,16 +144,17 @@ public class GreedyMCPlayer implements PokerSquaresPlayer {
     }
 
     /**
-     * From the chosen play, perform simulated Card draws and greedy placement (depthLimit) iterations forward 
-     * and return the resulting grid score.
+     * From the chosen play, perform simulated Card draws and greedy placement
+     * (depthLimit) iterations forward and return the resulting grid score.
+     *
      * @param depthLimit - how many simulated greedy plays to perform
-     * @return resulting grid score after greedy MC simulation to given depthLimit
+     * @return resulting grid score after greedy MC simulation to given
+     * depthLimit
      */
     private int simGreedyPlay(int depthLimit) {
         if (depthLimit == 0) { // with zero depth limit, return current score
             return system.getScore(grid);
-        }
-        else { // up to the non-zero depth limit or to game end, iteratively make the given number of greedy plays o
+        } else { // up to the non-zero depth limit or to game end, iteratively make the given number of greedy plays o
             int score = Integer.MIN_VALUE;
             int maxScore = Integer.MIN_VALUE;
             int depth = Math.min(depthLimit, NUM_POS - numPlays); // compute real depth limit, taking into account game end
@@ -158,8 +172,9 @@ public class GreedyMCPlayer implements PokerSquaresPlayer {
                     makePlay(card, play / SIZE, play % SIZE);
                     score = system.getScore(grid);
                     if (score >= maxScore) {
-                        if (score > maxScore)
+                        if (score > maxScore) {
                             bestPlays.clear();
+                        }
                         bestPlays.add(play);
                         maxScore = score;
                     }
@@ -176,24 +191,26 @@ public class GreedyMCPlayer implements PokerSquaresPlayer {
             return maxScore;
         }
     }
-    
+
     public void makePlay(Card card, int row, int col) {
         // match simDeck to event
         int cardIndex = numPlays;
-        while (!card.equals(simDeck[cardIndex]))
+        while (!card.equals(simDeck[cardIndex])) {
             cardIndex++;
+        }
         simDeck[cardIndex] = simDeck[numPlays];
         simDeck[numPlays] = card;
-        
+
         // update plays to reflect chosen play in sequence
         grid[row][col] = card;
         int play = row * SIZE + col;
         int j = 0;
-        while (plays[j] != play)
+        while (plays[j] != play) {
             j++;
+        }
         plays[j] = plays[numPlays];
         plays[numPlays] = play;
-        
+
         // increment the number of plays taken
         numPlays++;
     }
@@ -201,7 +218,7 @@ public class GreedyMCPlayer implements PokerSquaresPlayer {
     public void undoPlay() { // undo the previous play
         numPlays--;
         int play = plays[numPlays];
-        grid[play / SIZE][play % SIZE] = null;  
+        grid[play / SIZE][play % SIZE] = null;
     }
 
     /* (non-Javadoc)
@@ -222,6 +239,7 @@ public class GreedyMCPlayer implements PokerSquaresPlayer {
 
     /**
      * Demonstrate GreedyMCPlay with Ameritish point system.
+     *
      * @param args (not used)
      */
     public static void main(String[] args) {
--- src/RandomPlayer.java
+++ src/RandomPlayer.java
@@ -1,36 +1,38 @@
+
 import java.util.Collections;
 import java.util.Stack;
 
 /**
- * RandomPlayer - a simple example implementation of the player interface for PokerSquares that 
- * makes random placements.
- * Author: Todd W. Neller
+ * RandomPlayer - a simple example implementation of the player interface for
+ * PokerSquares that makes random placements. Author: Todd W. Neller
  */
 public class RandomPlayer implements PokerSquaresPlayer {
 
     /**
-     * a stack of all unchosen plays represented as row-major indices. Row-major indices: play (r, c) is recorded as a single integer r * SIZE + c.
-     * (See <a href="http://en.wikipedia.org/wiki/Row-major_order">http://en.wikipedia.org/wiki/Row-major_order</a>.)
+     * a stack of all unchosen plays represented as row-major indices. Row-major
+     * indices: play (r, c) is recorded as a single integer r * SIZE + c. (See
+     * <a href="http://en.wikipedia.org/wiki/Row-major_order">http://en.wikipedia.org/wiki/Row-major_order</a>.)
      */
-    private Stack<Integer> plays = new Stack<Integer>(); 
-    
+    private Stack<Integer> plays = new Stack<Integer>();
+
     /* (non-Javadoc)
      * @see PokerSquaresPlayer#setPointSystem(PokerSquaresPointSystem, long)
      */
     @Override
     public void setPointSystem(PokerSquaresPointSystem system, long millis) {
-        // The RandomPlayer cares about as much about the score system as a honey badger.   
+        // The RandomPlayer cares about as much about the score system as a honey badger.
     }
-    
+
     /* (non-Javadoc)
      * @see PokerSquaresPlayer#init()
      */
     @Override
     public void init() { // Create a stack of a random permutation of 25 play positions (0-24) for random plays
         plays.clear();
-        for (int i = 0; i < 25; i++)
+        for (int i = 0; i < 25; i++) {
             plays.push(i);
-        Collections.shuffle(plays); 
+        }
+        Collections.shuffle(plays);
     }
 
     /* (non-Javadoc)
@@ -53,6 +55,7 @@ public class RandomPlayer implements PokerSquaresPlayer {
 
     /**
      * Demonstrate RandomPlayer play with Ameritish point system.
+     *
      * @param args (not used)
      */
     public static void main(String[] args) {
