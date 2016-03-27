cbb3123020db99375d59cc02f9504a9d4d8a5e19
Meng <edmonluan@gmail.com>
2016-03-27 01:55:02 -0300

    fix bugs

--- src/MengYaXiPlayer.java
+++ src/MengYaXiPlayer.java
@@ -425,7 +425,7 @@ public final class MengYaXiPlayer implements PokerSquaresPlayer {
         for (final Candidate c : candidates) {
             if (c.getRoundScore() > maxRoundScore) {
                 maxRoundScore = c.getRoundScore();
-            } else if (c.getRoundScore() < maxRoundScore) {
+            } else if (c.getRoundScore() < minRoundScore) {
                 minRoundScore = c.getRoundScore();
             }
         }
