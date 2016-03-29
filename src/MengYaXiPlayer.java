0d560ad8cbd531c661ae322f71372102811803be
Meng <edmonluan@gmail.com>
2016-03-28 22:54:13 -0300

    tune

--- src/MengYaXiPlayer.java
+++ src/MengYaXiPlayer.java
@@ -16,7 +16,7 @@ import util.Linear;
 public final class MengYaXiPlayer implements PokerSquaresPlayer {
 
     private static final Linear QUOTA = new Linear(2, 1, 15, 0.45);
-    private static final Linear AWARD_FACTOR = new Linear(2, 0.0005, 6, 0.02);
+    private static final Linear AWARD_FACTOR = new Linear(2, 0.001, 6, 0.02);
     private static final int TARGET_SHUFFLES = 1000;
 
     public boolean verbose = false;
