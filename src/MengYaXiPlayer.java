27647e1a0051735b2148af26828ca08a90a22c95
Meng <edmonluan@gmail.com>
2016-04-01 17:45:32 -0300

    move the useful main into our code

--- src/MengYaXiPlayer.java
+++ src/MengYaXiPlayer.java
@@ -1,4 +1,6 @@
 
+import java.util.Scanner;
+
 /**
  *
  * @author Meng
@@ -13,5 +15,37 @@ public final class MengYaXiPlayer extends mengyaxi.pokersquares.PokerSquaresPlay
     @Override
     public int[] getPlay(final Card card, final long millisRemaining) {
         return super.getPlay(mengyaxi.pokersquares.Card.getCardById(card.getCardId()), millisRemaining);
+    }
+
+    public static void main(final String[] args) {
+        final MengYaXiPlayer player = new MengYaXiPlayer();
+
+        int times = 1;
+        long seed = System.currentTimeMillis();
+        int argn = 0;
+        boolean interactive = false;
+        for (final String arg : args) {
+            if (arg.equals("-v")) {
+                player.verbose = true;
+            } else if (arg.equals("-s")) {
+                player.parallel = false;
+            } else if (arg.equals("-i")) {
+                interactive = true;
+            } else if (argn == 0) {
+                times = Integer.parseUnsignedInt(arg);
+                ++argn;
+            } else {
+                seed = Long.parseUnsignedLong(arg);
+                ++argn;
+            }
+        }
+
+        final PokerSquares game = new PokerSquares(player, PokerSquaresPointSystem.getAmericanPointSystem());
+        if (interactive) {
+            //game.setSeed(times + seed);
+            game.play(new Scanner(System.in));
+        } else {
+            game.playSequence(times, seed, player.verbose);
+        }
     }
 }
